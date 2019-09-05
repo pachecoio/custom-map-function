26e61b23aa2e7c8f3ebcb0795a829542517da619
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2019-09-05 17:02:37 -0300

    Added index

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ Array.prototype.customMap = function (f) {
   const head = (arr) => arr[0];
   const tail = (arr) => arr.splice(1, this.length);
 
+  let currentIndex = -1;
+
   const map = (arr, f) => {
     if(!arr || arr.length === 0) return [];
     if(arr.length === 1) return [
-      f(head(arr)),
+      f(head(arr), ++currentIndex),
     ]
     return [
-      f(head(arr)),
+      f(head(arr), ++currentIndex),
       ...map(tail(arr), f)
     ]
   }
@@ -20,9 +22,10 @@ let arr = [
   1, 2, 3
 ]
 
-let newArr = arr.customMap((i) => {
-  console.log('current item', i);
-  return i;
+let newArr = arr.customMap((item, index) => {
+  console.log('current item', item);
+  console.log('current index', index);
+  return item;
 });
 
 console.log(newArr);
